9e92bb019cd642540e40fb07903ec439d1e1ab6c
Nathan Lilienthal <nathan@nixpulvis.com>
2018-01-22 18:16:08 -0700

    X/Y code test working.

--- main.c
+++ main.c
@@ -1,23 +1,46 @@
 #include <avrm.h>
 #include <avrm/shift.h>
 
+#define ROWS 3
+
 int main(void) {
     // Setup the shift register array.
     ShiftLatchConfig config = { 11, 12, 8 };
     shift_latch_init(config);
 
-    for (;;) {
-        // Shift out the binary from 0 to 255 each 100ms.
-        for (byte i = 0; i < 256; i++) {
-            byte buff[2] = {
-                i % 2 == 0 ? 1 : 2,
-                i % 2 == 0 ? i : 255-i
-            };
+    // `io` contains the two dimensions of an individual LED layer.
+    // The format is as follows:
+    // [ anode pin (MUST only be one bit set at a time),
+    //   cathode config (ANY arrangement of pins may be used) ]
+    // As dictated by hardware, these config registers are active LOW,
+    // meaning a value of 0x00 in both the anode and cathode config
+    // would turn the entire matrix on at once (don't do this).
+    byte io[2];
 
-            // XXX: We need a way to delay the buff[1] loop, but not the
-            // buff[0] loop.
-            shift_latch(config, buff, 2);
-            delay_ms(100);
+    for (;;) {
+        // Each LED one at a time.
+        for (int i = 0; i < ROWS; i++)
+        for (int j = 0; j < 8; j++) {
+            io[0] = ~(0x01<<i);
+            io[1] = ~(0x01<<j);
+            shift_latch(config, io, 2);
+            delay_ms(50);
+        }
+        // Each row at a time.
+        for (int i = 0; i < ROWS; i++) {
+            io[0] = ~(0x01<<i);
+            // NOTE: This is safe to completly enable because each cathod is
+            // attached to a pin able to sink it's ~20mA.
+            io[1] = 0x00;
+            shift_latch(config, io, 2);
+            delay_ms(400);
+        }
+        // Fast scanning.
+        for (int n = 0; n < 3000; n++)
+        for (int i = 0; i < ROWS; i++) {
+            io[0] = ~(0x01<<i);
+            io[1] = 0x42;
+            shift_latch(config, io, 2);
         }
     }
 
