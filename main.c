016151120b0dc151e9994f511f856386179c7ff4
Nathan Lilienthal <nathan@nixpulvis.com>
2018-01-26 12:23:48 -0700

    Updating with an ISR to seperate the scanning code.

--- main.c
+++ main.c
@@ -1,14 +1,59 @@
-#include <limits.h>
+#include <string.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
 #include <avrm.h>
 #include <avrm/shift.h>
 
-#define ROWS 8
+// `display` contains an 8x8 array of booleans for the state of the
+// corrisponding LED. The origin is the bottom left.
+static bool display[8][8];
+
+void update();
+void scan(ShiftLatchConfig, bool[8][8]);
+byte to_byte(bool[8]);
+
+ISR(TIMER1_OVF_vect) {
+    // Update the display buffer.
+    update();
+    // Reset the timer.
+    TCNT1 = 63974;
+}
 
 int main(void) {
+    // Setup the ISR timer.
+    TCNT1  = 63974;
+    TCCR1A = 0x00;
+    TCCR1B = (1<<CS10) | (1<<CS12);
+    TIMSK1 = (1 << TOIE1);
+    sei();
+
+    // Setup the inital display state.
+    display[0][0] = TRUE;
+
     // Setup the shift register array.
     ShiftLatchConfig config = { 5, 6, 7 };
     shift_latch_init(config);
 
+    for (;;) {
+        scan(config, display);
+    }
+
+    return 0;
+}
+
+void update() {
+    byte new[8][8];
+    for (int i = 0; i < 8; i++)
+    for (int j = 0; j < 8; j++)
+        new[i][j] = display[i][j - 1];
+    memcpy(display, new, sizeof(display));
+
+    if (display[2][1]) {
+        display[0][0] = TRUE;
+    }
+}
+
+void scan(ShiftLatchConfig config, bool display[8][8]) {
     // `io` contains the two dimensions of an individual LED layer.
     // The format is as follows:
     // [ anode pin (MUST only be one bit set at a time),
@@ -18,28 +63,23 @@ int main(void) {
     // would turn the entire matrix on at once (don't do this).
     byte io[2];
 
-    // `count` a number we're baseing the display on.
-    byte count = 0;
+    for (int i = 0; i < 8; i++) {
+        // The anode byte.
+        io[0] = ~(0x01<<i);
 
-    // An incremental number to base count on.
-    int n = 0;
+        // The cathode byte.
+        io[1] = ~(to_byte(display[i]));
 
-    for (;;) {
-        if (n % 50 == 0)
-            count++;
-
-        for (int i = 0; i < ROWS; i++) {
-            io[0] = ~(0x01<<i);
-            if (i % 2 == 0)
-                io[1] = count;
-            else
-                io[1] = 255 - count;
-            shift_latch(config, io, 2);
-        }
-
-        n++;
+        // Shift this row of the scanned display.
+        shift_latch(config, io, 2);
     }
+}
 
-    return 0;
+byte to_byte(bool b[8]) {
+    unsigned char c = 0;
+    for (int i=0; i < 8; ++i)
+        if (b[i])
+            c |= 1 << i;
+    return c;
 }
 
